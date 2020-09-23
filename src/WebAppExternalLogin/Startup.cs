9f392bd3988f3d1bda663d2ea2061af2dc9fc785
Herb Stahl <ghstahl@gmail.com>
2020-09-23 08:53:35 -0700

    services.AddDbContext<ApplicationDbContext>(config =>             {                 // for in memory database                 config.UseInMemoryDatabase("InMemoryIdentityDatabase");             });
    App is now using the InMemoryDatabase

--- src/WebAppExternalLogin/Startup.cs
+++ src/WebAppExternalLogin/Startup.cs
@@ -27,9 +27,11 @@ namespace WebAppExternalLogin
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ApplicationDbContext>(config =>
+            {
+                // for in memory database  
+                config.UseInMemoryDatabase("InMemoryIdentityDatabase");
+            });
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
