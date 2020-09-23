da1088bab0b00f7835a7ca8bdc25cc19f3aa9e6c
Herb Stahl <ghstahl@gmail.com>
2020-09-23 09:13:40 -0700

    Added external OpenId Connect IDP endpoints.
    Still using the full inmemory database
    
    Next is to Auto-Register and signin the user when they come in from the external IDP

--- src/WebAppExternalLogin/Startup.cs
+++ src/WebAppExternalLogin/Startup.cs
@@ -12,17 +12,22 @@ using WebAppExternalLogin.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebAppExternalLogin.Extensions;
 
 namespace WebAppExternalLogin
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(
+            IConfiguration configuration,
+            IHostEnvironment hostingEnvironment)
         {
             Configuration = configuration;
+            HostingEnvironment = hostingEnvironment;
         }
 
         public IConfiguration Configuration { get; }
+        public IHostEnvironment HostingEnvironment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -33,8 +38,14 @@ namespace WebAppExternalLogin
                 config.UseInMemoryDatabase("InMemoryIdentityDatabase");
             });
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddRazorPages();
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+            services.AddAuthentication<IdentityUser>(Configuration);
+            IMvcBuilder builder = services.AddRazorPages();
+            if (HostingEnvironment.IsDevelopment())
+            {
+                builder.AddRazorRuntimeCompilation();
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
